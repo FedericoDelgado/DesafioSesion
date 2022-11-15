701ad78fd3b7f19c738a03d58c30b5e0a11793ca
Federico Delgado <delgadofefe@gmail.com>
2022-11-15 19:55:38 -0300

    Desafio Sesion

---
+++ src/models/ModelsUsuarios.js
@@ -0,0 +1,25 @@
+import { config } from '../utils/config.js';
+import mongoose from "mongoose";
+import { usuariosModel } from '../model/usuarios.model.js';
+
+const strConn = config.atlasUsuarios.strConn;
+let objs;
+class ModelsUsuarios{
+    constructor(){}
+
+    async getUsuario(usuario){
+        try {
+            await mongoose.connect(strConn);
+            objs = await usuariosModel.findOne({username: { $eq: usuario }});
+            console.log('find:', objs)
+            return objs;
+           
+          } catch (error) {
+            console.log(error)
+        } finally {
+            await mongoose.disconnect()
+        } 
+    }
+}
+
+export default ModelsUsuarios;
